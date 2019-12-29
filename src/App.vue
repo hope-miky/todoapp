6673846c4bcf93ae47c57f1db7473070cf4e1c6d
Tesfamichael1074 <hope.miky1074@gmail.com>
2019-12-29 12:47:10 +0300

    integrated todos.vue

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,50 @@
 <template>
   <div id="app">
-   
+   <Todos />
   </div>
 </template>
 
 <script>
+import Todos from './components/Todos';
 
 export default {
   name: 'app',
   components: {
- 
+    Todos
+  },
+
+  data() {
+    return{
+      todos: [
+        {
+          id: 1,
+          tittle: "Todo one",
+          completed: false
+        },
+        {
+          id: 2,
+          tittle: "Todo two",
+          completed: true
+        },
+        {
+          id: 1,
+          tittle: "Todo three",
+          completed: false
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.4;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
 </style>
