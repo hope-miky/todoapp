688c1ed7a209a1f673e4d38bf82fc235f16a2847
Tesfamichael1074 <hope.miky1074@gmail.com>
2019-12-29 16:59:58 +0300

    adding to do

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
+    <Header />
+    <Addtodo v-on:add-todo="addTodo"/>
    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
 import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import Addtodo from './components/Addtodo';
 
 export default {
   name: 'app',
   components: {
-    Todos
+    Todos,
+    Header,
+    Addtodo,
   },
 
   data() {
@@ -37,7 +43,11 @@ export default {
   methods: {
     deleteTodo(id){
         this.todos = this.todos.filter(todo => todo.id != id);
-    }
+    },
+
+    addTodo(newTodo){
+      this.todos = [...this.todos, newTodo];
+    },
   }
 }
 </script>
@@ -52,4 +62,17 @@ export default {
     font-family: Arial, Helvetica, sans-serif;
     line-height: 1.4;
   }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
+  }
+
 </style>
---
+++ src/components/Addtodo.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <form @submit="addTodo">
+            <input type="text" v-model="tittle" name="tittle"  placeholder="Add Todo...">
+            <input type="sumit" value="Submit" class="btn">
+        </form>
+    </div>
+</template>
+
+
+<script>
+import uuid from 'uuid';
+
+
+export default {
+    name: "Addtodo",
+    data(){
+      return {
+          tittle: ''
+      }  
+    },
+    methods: {
+        addTodo(e){
+            e.preventDefault();
+            const newTodo = {
+                id: uuid.v4(),
+                tittle: this.tittle,
+                completed: false
+            }
+
+            // send the todo
+
+            this.$emit('add-todo', newTodo);
+        }
+    }
+}
+</script>
+
+
+<style  scoped>
+form {
+    display: flex;
+  }
+  input[type="text"] {
+    flex: 10;
+    padding: 5px;
+  }
+  input[type="submit"] {
+    flex: 2;
+  }
+</style>
